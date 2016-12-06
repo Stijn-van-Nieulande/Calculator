94b65a80bdb9257dcb87218730e9164bd12876ff
STIJN\stijn <99042075@davinci.nl>
2016-12-06 11:10:32 +0100

    update

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 var calcStringOLD = "";
 var calcStringNEW = "";
 var lastExpression = "";
+var somstring = "";
 var calculated = false;
 var first = true;
 var calculator = {};
@@ -11,18 +12,22 @@ calculator.add = function (s) {
 	} else {
 		calcStringNEW += s;
 	}
+	somstring += s;
 	calculated = false;
 	calculator.update();
 }
 calculator.execExpr = function (s) {
 	calculator.calc();
 	calculated = false;
+	somstring += (" " + s + " ");
 	lastExpression = s;
+	calculator.update(true);
 }
 calculator.clear = function () {
 	calcStringOLD = "";
 	calcStringNEW = "";
 	lastExpression = "";
+	somstring = "";
 	calculated = false;
 	first = true;
 	calculator.update();
@@ -51,7 +56,10 @@ calculator.calc = function () {
 	first = false;
 	calculator.update();
 }
-calculator.update = function () {
-	calc.value = (calculated || first) ? calcStringOLD : calcStringNEW;
-	calc.value = Math.round(calc.value * 1000) / 1000;
+calculator.update = function (somonly = false) {
+	if (!somonly) {
+		calc.value = (calculated || first) ? calcStringOLD : calcStringNEW;
+		calc.value = Math.round(calc.value * 1000) / 1000;
+	}
+	som.value = somstring;
 }
